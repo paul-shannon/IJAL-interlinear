e5573a988129c0259f41621d8229064b52d2086f
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-08-11 10:38:11 -0700

    lineLeadIn function now in line.py

--- design2/test_monkeyAndThunder.py
+++ design2/test_monkeyAndThunder.py
@@ -8,9 +8,10 @@ from degenerateLine import *
 from lineClassifier import *
 import importlib
 pd.set_option('display.width', 1000)
-
+import pdb
 
 filename = "../testData/monkeyAndThunder/AYA1_MonkeyandThunder.eaf"
+audioDirectory = "monkeyTest"
 xmlDoc = etree.parse(filename)
 lineCount = len(xmlDoc.findall("TIER/ANNOTATION/ALIGNABLE_ANNOTATION"))
 assert(lineCount == 41)
@@ -18,14 +19,12 @@ assert(lineCount == 41)
 htmlDoc = Doc()
 
 htmlDoc.asis('<!DOCTYPE html>')
+
 with htmlDoc.tag('html', lang="en"):
      with htmlDoc.tag('head'):
          htmlDoc.asis('<meta charset="UTF-8">')
          htmlDoc.asis('<link rel="stylesheet" href="ijal.css">')
          htmlDoc.asis('<script src="ijalUtils.js"></script>')
-#         htmlDoc.asis('<script>')
-#         htmlDoc.asis('function playSample(audioID){console.log(audioID); document.getElementById(audioID).play();}')
-#         htmlDoc.asis('</script>')
          with htmlDoc.tag('body'):
              for i in range(lineCount):
                  x = Line(xmlDoc, i)
@@ -36,13 +35,7 @@ with htmlDoc.tag('html', lang="en"):
                      classification = classifier.run()
                      print("%d: %s" % (i, classification))
                      with htmlDoc.tag("div", klass="line-sidebar"):
-                         htmlDoc.text("%d)" % (i + 1))
-                         audioTag = '<audio id="%s"><source src="monkeyTest/%s.wav"/></audio>' % (lineID, lineID)
-                         #print(audioTag)
-                         htmlDoc.asis(audioTag)
-                         buttonTag = '<button onclick="playSample(\'%s\')"><img src="https://www.americanlinguistics.org/wp-content/uploads/speaker.png"/></button>' % lineID
-                         #print(buttonTag)
-                         htmlDoc.asis(buttonTag)
+                         lineLeadIn(htmlDoc, i, lineID, audioDirectory)
                      if(classification == "CanonicalLine"):
                          xc = CanonicalLine(xmlDoc, i)
                          xc.toHtml(htmlDoc)
