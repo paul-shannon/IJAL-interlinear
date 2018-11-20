1010dfeb091d0fb960f5802e326b4f1fbf7efc01
paul-shannon <paul.thurmond.shannon@gmail.com>
2018-11-19 17:50:35 -0800

    dash upload example

---
+++ webapp/experiments/dash-upload/countCharactersInFile.py
@@ -0,0 +1,57 @@
+import datetime
+import base64
+import pdb
+
+import dash
+from dash.dependencies import Input, Output, State
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.scripts.config.serve_locally = True
+
+app.layout = html.Div([
+    dcc.Upload(
+        id='upload-image',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-image-upload'),
+])
+
+
+def parse_contents(contents, filename, date):
+   print("filename selected: %s" % filename)
+   #pdb.set_trace()
+   content_type, content_string = contents.split(',')
+   nchar = len(content_string)
+   print("%s (%s): %d" % (filename, content_type, nchar))
+   return(nchar)
+
+@app.callback(Output('output-image-upload', 'children'),
+              [Input('upload-image', 'contents')],
+              [State('upload-image', 'filename'),
+               State('upload-image', 'last_modified')])
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
